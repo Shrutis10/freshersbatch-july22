e946550fd9d0e7054eb15aa926e13020240164b7
Shrutis10 <87119638+Shrutis10@users.noreply.github.com>
2022-08-23 17:51:33 +0530

    Add files via upload

---
+++ ASP Assignments/Assignment1/WonMatchDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_1.Models
+{
+    public class WonMatchDetails
+    {
+        footballEntities entities=new footballEntities();
+        public List<FOOTBALL_LEAGUE> winninglist()
+        {
+            return entities.WonMatchDetails().ToList();
+        }
+    }
+}
