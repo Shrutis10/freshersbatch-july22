a6ce0f7fd1484ab2a6aae56fb5a37916523d3219
Shrutis10 <87119638+Shrutis10@users.noreply.github.com>
2022-08-23 17:59:50 +0530

    Commited Files

---
+++ ASP_Assignments/Assignment2/Models/BusInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment_2.Models
+{
+    [Table("BusInfo")]
+    public class BusInfo
+    {
+        public int BusID { get; set; }
+        public string BoardingPoint { get; set; }
+        public DateTime TravelDate { get; set; }
+        public double Amount { get; set; }
+        public int Rating { get; set; }
+    }
+}
